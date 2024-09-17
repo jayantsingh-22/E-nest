c98e3f91a9bdfba9ce979bbbfd4ce887edd4b46d
jayantsingh-22 <jayantsingh640@gmail.com>
2024-09-17 20:54:38 +0530

    fixed callStack errrors

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,5 +1,4 @@
 import type { Config } from "tailwindcss";
-
 const config: Config = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
@@ -33,8 +32,8 @@ const config: Config = {
       boxShadow: {
         light: "rgba(99, 99, 99, 0.1) 0px 1px 5px 1px;",
       },
+      },
     },
-  },
   plugins: [],
 };
 export default config;
